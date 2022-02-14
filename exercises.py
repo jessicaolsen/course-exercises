b4eb6c2ba4d0bd4fb3b623cf1af34f4233d9effb
Jessica Olsen <jaolsen@Jessicas-MacBook-Pro.local>
2022-02-14 10:28:55 -0500

    Initial Commit

---
+++ exercises.py
@@ -0,0 +1,16 @@
+#Exercise 1 Print Hello World
+#print('Hello World')
+
+#Exercise 2 Creating a Shape
+#print("   /|")
+#print("  / |")
+#print(" /  |")
+#print("/___|")
+
+#Exercise 3 Using Variables and Data Types
+character_name = "John"
+character_age = "35"
+print("There once was a man named "+ character_name + ",")
+print("he was 70 years old.")
+print("He really liked the name George, ")
+print("but didn't like being 70.")
